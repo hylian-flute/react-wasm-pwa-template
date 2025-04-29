30fc972177957b0464cf07bd3a6a16ddf23b23eb
hylian-flute <hylian_flute@yahoo.co.jp>
2025-04-29 15:18:34 +0900

    fix: フロントエンドからwasmのシンボリックリンクではなくコピーを使う

--- build-wasm.sh
+++ build-wasm.sh
@@ -4,3 +4,4 @@ pushd ./wasm
 cargo build --target wasm32-unknown-unknown --release && \
   wasm-bindgen --target web --out-dir ./pkg ./target/wasm32-unknown-unknown/release/wasm.wasm
 popd
+cp ./wasm/pkg/wasm_bg.wasm ./public/wasm_bg.wasm
